f9bc0d7008ecf82d6a801d7881b0b78d0350d72b
PhilippeAssis <lippejc@gmail.com>
2015-03-20 19:12:43 -0300

    Initial commit

---
+++ install.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import os
+os.system("sudo ln host /usr/local/bin/devhost")
+os.system("sudo ln site /usr/local/bin/devsite")
+print """
+ __        __   _       ____               _____           _
+ \ \      / /__| |__   |  _ \  _____   __ |_   _|__   ___ | |___
+  \ \ /\ / / _ \ '_ \  | | | |/ _ \ \ / /   | |/ _ \ / _ \| / __|
+   \ V  V /  __/ |_) | | |_| |  __/\ V /    | | (_) | (_) | \__ \\
+    \_/\_/ \___|_.__/  |____/ \___| \_/     |_|\___/ \___/|_|___/
+"""
+print "In"
